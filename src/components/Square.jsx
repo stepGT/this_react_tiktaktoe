acf8250fe63e617881500065a09071c337f607d7
Andrey <stepgt@mail.ru>
2022-08-04 15:04:57 +1000

    Added style app && state Square

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,12 @@
-const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" onClick={() => setValue('X')}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
