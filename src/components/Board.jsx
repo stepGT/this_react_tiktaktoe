61c527eaca2dbaa3d6fd8b8ad988a3593b112842
Andrey <stepgt@mail.ru>
2022-08-05 14:07:32 +1000

    Added calculateWinner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -11,9 +11,30 @@ const Board = () => {
     setSquares(items);
     setXIsNext(!xIsNext);
   };
-  const renderSquare = i => <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const renderSquare = (i) => <Square value={squares[i]} onClick={() => handleClick(i)} />;
 
-  const status = 'Next player: ' + (xIsNext ? 'X' : '0');
+  const winner = calculateWinner(squares);
+  let status;
+  winner ? (status = 'Winner: ' + winner) : (status = 'Next player: ' + (xIsNext ? 'X' : 'O'));
 
   return (
     <div>
